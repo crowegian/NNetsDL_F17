1808d7ec432e312ee947da9599612c9bfffc53ef
crowegian <oliverbdw4@gmail.com>
2017-10-26 14:17:09 -0400

    added some edits to my own cnn

--- assignment2/ecbm4040/neuralnets/cnn.py
+++ assignment2/ecbm4040/neuralnets/cnn.py
@@ -151,8 +151,8 @@ class fc_layer(object):
 
 def my_LeNet(input_x, input_y,
           img_len=32, channel_num=3, output_size=10,
-          conv_featmap=[6, 16], fc_units=[84],
-          conv_kernel_size=[5, 5], pooling_size=[2, 2],
+          conv_featmap=[[6, 16]], fc_units=[84],
+          conv_kernel_size=[[5, 5]], pooling_size=[2, 2],
           l2_norm=0.01, seed=235):
     """
         LeNet is an early and famous CNN architecture for image classfication task.
@@ -169,44 +169,106 @@ def my_LeNet(input_x, input_y,
 
         http://deeplearning.net/tutorial/lenet.html
 
+        TODO:
+            1) Probably going to run into sizing issues when you go from pooling to different layers
+                as this will change size so you need to do some checks before running to be sure
+                the sizes will match up
     """
+    # indices in conv_featmap and conv_kernel_size need to match up.
+    # What we should do is change them though to be lists of lists
+    # so we can have the patter [[conv -> relu]*N -> Pool]*M where 
+    # len(pooling_size) = M = len(conv_kernel_size) = len(conv_featmap) 
+    # and len(conv_featmap[i]) == len(conv_kernel_size[i]) and can be variable.
+    # each inner list in conv_* represents how many conv relus to apply.
 
     assert len(conv_featmap) == len(conv_kernel_size) and len(conv_featmap) == len(pooling_size)
+    for idx, convReluKernels in enumerate(conv_kernel_size):
+        assert len(conv_kernel_size[idx]) == len(conv_featmap[idx])
+
+
+    conv_w = []
+    currInputConv = input_x
+    currChannelNum = channel_num
+    layerIdx = 0
+    for N_i, _ in enumerate(pooling_size):# iterate through all outer pooling layers which follow [conv -> relu]*N_i
+        for M_i, _ in enumerate(conv_featmap[N_i]):# iterate through all conv -> relu layers and apply them
+            convLayer = conv_layer(input_x = currInputConv,
+                              in_channel = currChannelNum,
+                              out_channel = conv_featmap[N_i][M_i],
+                              kernel_shape = conv_kernel_size[N_i][M_i],
+                              rand_seed=seed,
+                              index = layerIdx)
+            currInputConv = convLayer.output()
+            currChannelNum = conv_featmap[N_i][M_i]
+            conv_w.append(convLayer.weight)
+            layerIdx += 1
+        pooling_layer = max_pooling_layer(input_x=convLayer.output(),
+                                        k_size=pooling_size[N_i],
+                                        padding="VALID")
+        currInputConv = pooling_layer.output()# TODO this seems like it'll work but let's see
+
+
+
+
 
     # conv layer
-    conv_layer_0 = conv_layer(input_x=input_x,
-                              in_channel=channel_num,
-                              out_channel=conv_featmap[0],
-                              kernel_shape=conv_kernel_size[0],
-                              rand_seed=seed)
 
-    pooling_layer_0 = max_pooling_layer(input_x=conv_layer_0.output(),
-                                        k_size=pooling_size[0],
-                                        padding="VALID")
+    # conv_layer_0 = conv_layer(input_x=input_x,
+    #                           in_channel=channel_num,
+    #                           out_channel=conv_featmap[0],
+    #                           kernel_shape=conv_kernel_size[0],
+    #                           rand_seed=seed)
+
+    # pooling_layer_0 = max_pooling_layer(input_x=conv_layer_0.output(),
+    #                                     k_size=pooling_size[0],
+    #                                     padding="VALID")
 
     # flatten
-    pool_shape = pooling_layer_0.output().get_shape()
+    pool_shape = pooling_layer.output().get_shape()
     img_vector_length = pool_shape[1].value * pool_shape[2].value * pool_shape[3].value
-    flatten = tf.reshape(pooling_layer_0.output(), shape=[-1, img_vector_length])
+    flatten = tf.reshape(pooling_layer.output(), shape=[-1, img_vector_length])
 
+    print("Input shape for fully connected layer: {}".format(flatten.get_shape()))
+    fc_w = []
+    currInputFC = flatten
+    currInputSize = img_vector_length
+    for idx, layerSize in enumerate(fc_units):
+        if (idx + 1) == len(fc_units):
+            # reached final layer so don't use an activation function
+            fcLayer = fc_layer(input_x=currInputFC,
+                              in_size=currInputSize,
+                              out_size=output_size,
+                              rand_seed=seed,
+                              activation_function=None,
+                              index=idx)
+        else:
+            fcLayer = fc_layer(input_x=currInputFC,
+                              in_size=currInputSize,
+                              out_size=layerSize,
+                              rand_seed=seed,
+                              activation_function=tf.nn.relu,
+                              index=idx)
+        currInputFC = fcLayer.output()
+        currInputSize = layerSize
+        fc_w.append(fcLayer.weight)
     # fc layer
-    fc_layer_0 = fc_layer(input_x=flatten,
-                          in_size=img_vector_length,
-                          out_size=fc_units[0],
-                          rand_seed=seed,
-                          activation_function=tf.nn.relu,
-                          index=0)
+    # fc_layer_0 = fc_layer(input_x=flatten,
+    #                       in_size=img_vector_length,
+    #                       out_size=fc_units[0],
+    #                       rand_seed=seed,
+    #                       activation_function=tf.nn.relu,
+    #                       index=0)
 
-    fc_layer_1 = fc_layer(input_x=fc_layer_0.output(),
-                          in_size=fc_units[0],
-                          out_size=output_size,
-                          rand_seed=seed,
-                          activation_function=None,
-                          index=1)
+    # fc_layer_1 = fc_layer(input_x=fc_layer_0.output(),
+    #                       in_size=fc_units[0],
+    #                       out_size=output_size,
+    #                       rand_seed=seed,
+    #                       activation_function=None,
+    #                       index=1)
 
     # saving the parameters for l2_norm loss
-    conv_w = [conv_layer_0.weight]
-    fc_w = [fc_layer_0.weight, fc_layer_1.weight]
+    # conv_w = [conv_layer_0.weight]
+    # fc_w = [fc_layer_0.weight, fc_layer_1.weight]
 
     # loss
     with tf.name_scope("loss"):
@@ -215,13 +277,13 @@ def my_LeNet(input_x, input_y,
 
         label = tf.one_hot(input_y, 10)
         cross_entropy_loss = tf.reduce_mean(
-            tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=fc_layer_1.output()),
+            tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=fcLayer.output()),
             name='cross_entropy')
         loss = tf.add(cross_entropy_loss, l2_norm * l2_loss, name='loss')
 
         tf.summary.scalar('LeNet_loss', loss)
 
-    return fc_layer_1.output(), loss
+    return fcLayer.output(), loss
 
 
 def cross_entropy(output, input_y):
